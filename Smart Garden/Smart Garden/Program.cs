ce9059847c28dc97a9768e93f68c37f6a3b3fb60
pwr227078 <227078@student.pwr.edu.pl>
2018-11-18 15:02:24 +0100

    New project

---
+++ Smart Garden/Smart Garden/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Smart_Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
